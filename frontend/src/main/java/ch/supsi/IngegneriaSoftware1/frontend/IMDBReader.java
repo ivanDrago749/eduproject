1599cfbaca2af047a428497f9bbd352b122d6a28
TrotaCazzuta <gimmy.tazza@gmail.com>
2025-03-10 09:02:05 +0100

    stampa film per direttore

--- frontend/src/main/java/ch/supsi/IngegneriaSoftware1/frontend/IMDBReader.java
+++ frontend/src/main/java/ch/supsi/IngegneriaSoftware1/frontend/IMDBReader.java
@@ -48,17 +48,4 @@ public class IMDBReader {
         }
         return moviesByDirector;
     }
-
-    public static void main(String[] args) {
-        String filePath = "/Users/simonecoggio/Desktop/SUPSI/IngegneriaDelSoftware/eduproject/imdb_top_1000.csv"; // Assicurati di impostare il percorso corretto del file
-        TreeMap<String, List<Film>> moviesByDirector = readCSV(filePath);
-
-        // Stampa il contenuto della TreeMap
-        for (String director : moviesByDirector.keySet()) {
-            System.out.println("Director: " + director);
-            for (Film movie : moviesByDirector.get(director)) {
-                System.out.println("  " + movie);
-            }
-        }
-    }
 }
---
+++ frontend/src/main/java/ch/supsi/IngegneriaSoftware1/frontend/Main.java
@@ -0,0 +1,19 @@
+package ch.supsi.IngegneriaSoftware1.frontend;
+import ch.supsi.IngegneriaSoftware1.backend.Film;
+import java.util.List;
+import java.util.TreeMap;
+public class Main{
+    public static void main(String[] args) {
+        IMDBReader imdbReader = new IMDBReader();
+        String filePath = "/Users/simonecoggio/Desktop/SUPSI/IngegneriaDelSoftware/eduproject/imdb_top_1000.csv"; // Assicurati di impostare il percorso corretto del file
+        TreeMap<String, List<Film>> moviesByDirector = imdbReader.readCSV(filePath);
+
+        // Stampa il contenuto della TreeMap
+        for (String director : moviesByDirector.keySet()) {
+            System.out.println("Director: " + director);
+            for (Film movie : moviesByDirector.get(director)) {
+                System.out.println("  " + movie);
+            }
+        }
+    }
+}
